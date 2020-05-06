8796c174df7e8e9896e55603db6eccbfb5ac9a0b
nikki <nikkimeadows1@gmail.com>
2020-05-06 15:28:30 +1200

    some more work on the ajax client.

--- accounts-ajax/public/js/testinghtml.js
+++ accounts-ajax/public/js/testinghtml.js
@@ -5,8 +5,27 @@
  */
 
 "use strict";
+/**
+ * Create customer module
+ */
 let module = angular.module('CustomerModule', ['ngResource']);
+let serviceURI = 'http://localhost:8080/api';
+
+
+module.factory('getCustomerApi', function ($resource)
+{ return $resource(serviceURI + '/customer'); });
+
+//customer
+module.factory('createCustomerApi', function ($resource) { 
+    return $resource(serviceURI + '/customer', 
+    null, {update: {method: 'POST'}}); });
+
+//jetty using a different port 
+module.factory('createCustomerApiJetty', function ($resource) { 
+    return $resource( 'http://localhost:9000/api'+ '/customer', 
+    null, {update: {method: 'POST'}}); });
+
+
 module.controller('CustomerController', function () {
-this.test = "It works!";
 });
 
