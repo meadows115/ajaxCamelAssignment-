ee439ee1871832b21a5d2cff6f01093c990dd1a7
nikki <nikkimeadows1@gmail.com>
2020-05-14 17:27:00 +1200

    dont think this is working. (the sales stuff).

--- router/src/main/java/router/CustomerSaleBuilder.java
+++ router/src/main/java/router/CustomerSaleBuilder.java
@@ -5,9 +5,11 @@
  */
 package router;
 
+import domain.Sale;
 import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 
 /**
  *
@@ -19,23 +21,31 @@ public class CustomerSaleBuilder extends RouteBuilder {
     public void configure() {
 
         // get Camel to look in my student email account for vend emails
-        from("imaps://outlook.office365.com?username=meani898@student.otago.ac.nz"
-                + "&password=" + getPassword("Enter your E-Mail password")
-                + "&searchTerm.subject=Vend:SaleUpdate"
-                + "&debugMode=false" // set to true if you want to see the authentication details
-                + "&folderName=INBOX") // change to whatever folder your Vend messages end up in
-                .convertBodyTo(String.class)
-                .log("${body}")
+//        from("imaps://outlook.office365.com?username=meani898@student.otago.ac.nz"
+//                + "&password=" + getPassword("Enter your E-Mail password")
+//                + "&searchTerm.subject=Vend:SaleUpdate"
+//                + "&debugMode=false" // set to true if you want to see the authentication details
+//                + "&folderName=INBOX") // change to whatever folder your Vend messages end up in
+//                .convertBodyTo(String.class)
+//                .log("${body}")
+//                .to("jms:queue:vend-new-sale");
+        from("imap://localhost?username=test@localhost"
+                + "&port=3143"
+                + "&password=password"
+                + "&consumer.delay=5000"
+                + "&searchTerm.subject=Vend:SaleUpdate")
+                .log("Found new E-Mail: ${body}")
                 .to("jms:queue:vend-new-sale");
-//        from("imap://localhost?username=test@localhost"
-//                + "&port=3143"
-//                + "&password=password"
-//                + "&consumer.delay=5000"
-//                + "&searchTerm.subject=<subject>")
-//                .log("Found new E-Mail: ${body}")
-//                .to("jms:queue:messages");
-    }
 
+        //extract the customers group, id, first name, last name and email
+        from("jms:queue:vend-new-sale")
+              .setHeader("group").jsonpath("$.customer.customer_group_id")  // extract ID from JSON, and store in header 
+              .setHeader("firstName").jsonpath("$.customer.first_name") // same for name 
+              .setHeader("lastName").jsonpath("$.customer.last_name") 
+              .setHeader("email").jsonpath("$.customer.email")
+              .setHeader("id").jsonpath("$.customer.id") 
+              .to("jms:queue:extracted-properties");
+    }
     //method to prompt for a password using the dialog box
     public static String getPassword(String prompt) {
         JPasswordField txtPasswd = new JPasswordField();
