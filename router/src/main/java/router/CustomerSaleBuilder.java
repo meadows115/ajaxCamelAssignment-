c202fd5fa4c9890ec195463c6d867288aaee6eba
nikki <nikkimeadows1@gmail.com>
2020-05-19 13:20:50 +1200

    All seems to be working, need to do the vend queue next.

--- router/src/main/java/router/CustomerSaleBuilder.java
+++ router/src/main/java/router/CustomerSaleBuilder.java
@@ -25,21 +25,21 @@ public class CustomerSaleBuilder extends RouteBuilder {
     public void configure() {
 
         // get Camel to look in my student email account for vend emails
-//        from("imaps://outlook.office365.com?username=meani898@student.otago.ac.nz"
-//                + "&password=" + getPassword("Enter your E-Mail password")
-//                + "&searchTerm.subject=Vend:SaleUpdate"
-//                + "&debugMode=false" // set to true if you want to see the authentication details
-//                + "&folderName=INBOX") // change to whatever folder your Vend messages end up in
-//                .convertBodyTo(String.class)
-//                .log("${body}")
-//                .to("jms:queue:vend-new-sale");
-        from("imap://localhost?username=test@localhost"
-                + "&port=3143"
-                + "&password=password"
-                + "&consumer.delay=5000"
-                + "&searchTerm.subject=Vend:SaleUpdate")
-                .log("Found new E-Mail: ${body}")
+        from("imaps://outlook.office365.com?username=meani898@student.otago.ac.nz"
+                + "&password=" + getPassword("Enter your E-Mail password")
+                + "&searchTerm.subject=Vend:SaleUpdate"
+                + "&debugMode=false" // set to true if you want to see the authentication details
+                + "&folderName=INBOX") // change to whatever folder your Vend messages end up in
+                .convertBodyTo(String.class)
+                .log("${body}")
                 .to("jms:queue:vend-new-sale");
+//        from("imap://localhost?username=test@localhost"
+//                + "&port=3143"
+//                + "&password=password"
+//                + "&consumer.delay=5000"
+//                + "&searchTerm.subject=Vend:SaleUpdate")
+//                .log("Found new E-Mail: ${body}")
+//                .to("jms:queue:vend-new-sale");
 
         //extract the customers group, id, first name, last name and email
         from("jms:queue:vend-new-sale")
@@ -116,7 +116,7 @@ public class CustomerSaleBuilder extends RouteBuilder {
                 // set HTTP method
                 .setHeader(Exchange.HTTP_METHOD, constant("PUT"))
                 .setHeader(Exchange.CONTENT_TYPE).constant("application/json")
-                .log("step 6 - ${body}") //works up until HERE
+                .log("step 6 - ${body}") 
                 .toD("http://localhost:8086/api/accounts/account/${exchangeProperty.id}")
                 .log("step 7 - ${body}")
                 .to("jms:queue:customer-account-updated");
